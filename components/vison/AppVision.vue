b5451c609e8edffc0b1a5d5b5553a54a067b518a
TopFusion-dev <ramet.top@gmail.com>
2020-10-19 13:01:51 +0700

    [UPDATE] success history information room

--- components/vison/AppVision.vue
+++ components/vison/AppVision.vue
@@ -1,34 +1,19 @@
 <template>
   <section>
+    <AppTitleParallax :title="titleParallax" :sub-title="subTitleParallax" />
     <v-layout column wrap class="my-12" align-center>
-      <v-flex xs12 sm4 class="my-4">
-        <div class="text-center">
-          <h2 class="headline mb-5">สาขาวิทยาการคอมพิวเตอร์</h2>
-
-          <!-- <span class="subheading">Vison CSBSRU</span> -->
-          <footer>
-            <small>
-              เริ่มต้นเส้นทางที่ดีที่สุดสำหรับ Developer
-            </small>
-            <br />
-            <small>
-              <em>&mdash;Vison CSBSRU&mdash;</em>
-            </small>
-          </footer>
-        </div>
-      </v-flex>
       <v-flex xs12>
         <v-container grid-list-xl>
           <v-layout row wrap align-center>
             <v-flex xs12 md4>
               <v-card flat class="transparent">
                 <v-card-text class="text-center">
-                  <v-icon x-large class="blue--text text--lighten-2"
+                  <v-icon x-large class="purple--text text--lighten-3"
                     >fab fa-sketch</v-icon
                   >
                 </v-card-text>
                 <v-card-title primary-title class="layout justify-center">
-                  <div class="headline text-center">ปรัชญา ( philosophy )</div>
+                  <div class="headline text-center">ปรัชญา ( Philosophy )</div>
                 </v-card-title>
                 <v-card-text class="text-center"
                   >"ความรู้ดี มีทักษะ มานะเยี่ยม เปี่ยมคุณธรรม
@@ -39,30 +24,21 @@
             <v-flex xs12 md4>
               <v-card flat class="transparent">
                 <v-card-text class="text-center">
-                  <v-icon x-large class="blue--text text--lighten-2"
+                  <v-icon x-large class="purple--text text--lighten-3"
                     >fas fa-code</v-icon
                   >
                 </v-card-text>
-                <!-- <v-card-title primary-title class="layout justify-center">
-                  <div class="headline text-center">พันธกิจ ( mission )</div>
-                </v-card-title>
-                <v-card-text class="text-center">
-                  <p>1. ด้านการจัดการศึกษา ผลิตบัณฑิตสาขาวิทยาศาสตร์ โปรแกรมวิชาวิทยาการคอมพิวเตอร์ ให้มีความรู้ ทักษะ จริยธรรม และคุณภาพในวิชาชีพ</p>
-                  <p>2. ด้านงานวิจัย ส่งเสริมและสนับสนุนให้นิสิต และนักศึกษาได้จัดทำงานวิจัยทางสาขาวิทยาการคอมพิวเตอร์ โดยนำองค์ความรู้ที่ได้ศึกษามาพัฒนางานเพื่อตอบสนองต่อความต้องการของท้องถิ่น และประเทศชาติ</p>
-                  <p>3. ด้านการบริการท้องถิ่น เป็นแหล่งให้บริการวิชาการ ความรู้ทางด้านของคอมพิวเตอร์ และมีส่วนร่วมในการนำเอาความรู้ทางสาขาวิทยาการคอมพิวเตอร์ไปใช้ในการพัฒนา และบริการแก่ท้องถิ่น</p>
-                  <p>4. ด้านศิลปวัฒนธรรม ส่งเสริมและสนับสนุนการจัดกิจกรรมทางด้านศิลปวัฒนธรรมอันดีงามของประเทศ และร่วมสนับสนุนทางด้านการทำนุบำรุงศิลปวัฒนธรรม และภูมิปัญญาไทย</p>
-                </v-card-text> -->
               </v-card>
             </v-flex>
             <v-flex xs12 md4>
               <v-card flat class="transparent">
                 <v-card-text class="text-center">
-                  <v-icon x-large class="blue--text text--lighten-2"
+                  <v-icon x-large class="purple--text text--lighten-3"
                     >fas fa-globe-europe</v-icon
                   >
                 </v-card-text>
                 <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">วิสัยทัศน์ ( vision )</div>
+                  <div class="headline">วิสัยทัศน์ ( Vision )</div>
                 </v-card-title>
                 <v-card-text class="text-center"
                   >ผลิตบัณฑิตในสาขาวิทยาการคอมพิวเตอร์ที่มีคุณธรรม จริยธรรม
@@ -72,6 +48,45 @@
               </v-card>
             </v-flex>
           </v-layout>
+
+          <v-row>
+            <v-card-title primary-title class="layout justify-center">
+              <div class="headline text-center">พันธกิจ ( Mission )</div>
+            </v-card-title>
+          </v-row>
+
+          <v-row justify="center">
+            <v-expansion-panels inset>
+              <v-expansion-panel style="background-color: #f3e5f5;">
+                <v-expansion-panel-header>รายละเอียด</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <p>
+                    1. ด้านการจัดการศึกษา ผลิตบัณฑิตสาขาวิทยาศาสตร์
+                    โปรแกรมวิชาวิทยาการคอมพิวเตอร์ ให้มีความรู้ ทักษะ จริยธรรม
+                    และคุณภาพในวิชาชีพ
+                  </p>
+                  <p>
+                    2. ด้านงานวิจัย ส่งเสริมและสนับสนุนให้นิสิต
+                    และนักศึกษาได้จัดทำงานวิจัยทางสาขาวิทยาการคอมพิวเตอร์
+                    โดยนำองค์ความรู้ที่ได้ศึกษามาพัฒนางานเพื่อตอบสนองต่อความต้องการของท้องถิ่น
+                    และประเทศชาติ
+                  </p>
+                  <p>
+                    3. ด้านการบริการท้องถิ่น เป็นแหล่งให้บริการวิชาการ
+                    ความรู้ทางด้านของคอมพิวเตอร์
+                    และมีส่วนร่วมในการนำเอาความรู้ทางสาขาวิทยาการคอมพิวเตอร์ไปใช้ในการพัฒนา
+                    และบริการแก่ท้องถิ่น
+                  </p>
+                  <p>
+                    4. ด้านศิลปวัฒนธรรม
+                    ส่งเสริมและสนับสนุนการจัดกิจกรรมทางด้านศิลปวัฒนธรรมอันดีงามของประเทศ
+                    และร่วมสนับสนุนทางด้านการทำนุบำรุงศิลปวัฒนธรรม
+                    และภูมิปัญญาไทย
+                  </p>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-row>
         </v-container>
       </v-flex>
     </v-layout>
@@ -79,7 +94,22 @@
 </template>
 
 <script>
-export default {}
+import AppTitleParallax from '@/components/parallax/AppTitleParallax'
+
+export default {
+  components: {
+    AppTitleParallax,
+  },
+
+  computed: {
+    titleParallax() {
+      return 'สาขาวิทยาการคอมพิวเตอร์'
+    },
+    subTitleParallax() {
+      return 'เริ่มต้นเส้นทางที่ดีที่สุดสำหรับ Developer'
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
